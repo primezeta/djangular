3330bb13a3432ebf6bc260ac8ecd847462ba09da
primezeta <zachary.ladlie@gmail.com>
2018-03-21 17:40:15 -0500

    Update to ModelViewSet for card and list APIs, add CSS for displaying cards, and update scrumboard to allow editing of cards

--- djangular/djangular/urls.py
+++ djangular/djangular/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'/', TemplateView.as_view(template_name="scrumboard/home.html")),
+    path(r'', TemplateView.as_view(template_name="scrumboard/home.html")),
     path('scrumboard/', include('scrumboard.urls'))
 ]
